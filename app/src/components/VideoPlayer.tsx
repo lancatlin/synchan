eabbd46ea0ff9b5443d3c0b088e5a554e01b394e
wancat <wancat@wancat.cc>
2024-07-05 22:48:16 +0800

    feat: load video and control the playback

--- app/src/components/VideoPlayer.tsx
+++ app/src/components/VideoPlayer.tsx
@@ -1,3 +1,54 @@
+import { useRef, useState } from "react";
+
 export default function VideoPlayer() {
-  return <div className="text-2xl">VideoPlayer</div>;
+  const videoRef = useRef<HTMLVideoElement>(null);
+  const [currentTime, setCurrentTime] = useState(0);
+
+  const handlePlay = () => {
+    if (videoRef.current) {
+      videoRef.current.play();
+    }
+  };
+
+  const handlePause = () => {
+    if (videoRef.current) {
+      videoRef.current.pause();
+    }
+  };
+
+  const handleForward = () => {
+    const newTime = currentTime + 10;
+    if (videoRef.current) {
+      videoRef.current.currentTime = newTime;
+      setCurrentTime(newTime);
+    }
+  };
+
+  const updateCurrentTime = () => {
+    if (videoRef.current) {
+      setCurrentTime(videoRef.current.currentTime);
+    }
+  };
+
+  return (
+    <div className="text-2xl">
+      <video
+        className="w-full h-full"
+        controls
+        ref={videoRef}
+        src="/videos/sample.mkv"
+        onTimeUpdate={updateCurrentTime}
+      />
+      {currentTime}
+      <button className="bg-blue-500 text-white p-2" onClick={handlePlay}>
+        Play
+      </button>
+      <button className="bg-blue-500 text-white p-2" onClick={handlePause}>
+        Pause
+      </button>
+      <button className="bg-blue-500 text-white p-2" onClick={handleForward}>
+        Forward
+      </button>
+    </div>
+  );
 }
